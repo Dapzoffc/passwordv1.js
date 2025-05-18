767007924ffed856a508e6d3681dc5e68c1e2a1d
Dapzoffc <akunffmpmax30@gmail.com>
2025-05-18 14:37:24 +0700

    Create passwordv1.js

---
+++ passwordv1.js
@@ -0,0 +1,17 @@
+{
+
+  "secretCode": "Version1 Gen1",
+
+  "description": "HARVARD CRASH V1GEN1", 
+
+  "lastUpdated": "23/04/2025",
+
+  "author": "Dapz & Corzza", 
+
+  "status": "Active",
+
+  "license": "MIT",
+
+  "version": "1.0"
+
+}
